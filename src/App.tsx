fb540f84f0db2a71e94db0d692cb21367b5587fe
大矢敏 <ohya@mbp-m4-max.local>
2025-03-16 09:24:26 +0900

    🔧 fix: ファイル名のtypoを修正
    block-editer.tsx → block-editor.tsx に修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import BlockEditor from './block-editer'
+import MonacoEditor from './monaco-editor'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <BlockEditor></BlockEditor>
+      <BlockEditor />
+      <MonacoEditor />
     </>
   )
 }
